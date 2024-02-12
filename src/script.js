62e39ff78a8330d776e6aa17dd928ca873448574
Davin <lampdavin@gmail.com>
2024-02-11 23:51:59 -0500

    Created state machine component

---
+++ src/script.js
@@ -0,0 +1,142 @@
+import { html, render } from "lit-html";
+
+// A simple lit-html directive to drive events from a driver
+const drive = (driver) => (event) => {
+  driver.transmit({ view: event });
+};
+
+class Driver {
+  constructor() {
+    this.receivers = [];
+  }
+
+  transmit(data) {
+    this.receivers.forEach((receive) => {
+      receive(data);
+    });
+  }
+
+  attachReceiver(receive) {
+    this.receivers.push(receive);
+  }
+}
+
+class StateMachine {
+  constructor(
+    initialState,
+    combineFunction = (input, state) => {
+      return {
+        output: input,
+        state: input,
+      };
+    },
+    initialOutput
+  ) {
+    this.resetState = () => initialState;
+    this.state = this.resetState();
+
+    this.resetOutput = () => initialOutput || initialState;
+    this.output = this.resetOutput();
+
+    this.combine = combineFunction;
+  }
+
+  reset = () => {
+    this.state = this.resetState();
+    this.output = this.resetOutput();
+  };
+
+  trigger = (input) => {
+    const { output, state } = this.combine(input, this.state);
+    this.state = state;
+    this.output = output;
+  };
+
+  getState = () => this.state;
+  getOutput = () => this.output;
+}
+
+class Controller {
+  constructor(stateMachine, triggerFunction = (input, state) => true) {
+    this.stateMachine = stateMachine;
+    this.triggerFunction = triggerFunction;
+    this.driver = new Driver();
+  }
+
+  reset = () => {
+    this.stateMachine.reset();
+    this.driver.transmit(this.stateMachine.getOutput());
+  };
+
+  receive = (input) => {
+    if (this.triggerFunction(input, this.stateMachine.getState())) {
+      this.stateMachine.trigger(input);
+      this.driver.transmit(this.stateMachine.getOutput());
+    }
+  };
+
+  getDriver = () => this.driver;
+}
+
+class View {
+  constructor(template, container = document.body) {
+    this.container = container;
+    this.driver = new Driver();
+    this.template = template;
+  }
+
+  receive = (input) => {
+    render(this.template(input, this.getDriver()), this.container);
+  };
+
+  getDriver = () => this.driver;
+}
+
+class Component {
+  constructor(viewTemplate, stateMachine) {
+    this.controller = new Controller(stateMachine);
+    this.view = new View(viewTemplate);
+    this.driver = new Driver();
+
+    const viewDriver = this.view.getDriver();
+    const controllerDriver = this.controller.getDriver();
+
+    viewDriver.attachReceiver(this.controller.receive);
+    controllerDriver.attachReceiver(this.view.receive);
+
+    controllerDriver.attachReceiver(this.forward);
+    this.reset();
+  }
+
+  reset = () => {
+    this.controller.reset();
+  };
+
+  receive = (input) => {
+    this.controller.receive({ input: input });
+  };
+
+  forward = (output) => {
+    this.driver.transmit(output);
+  };
+
+  getDriver = () => this.driver;
+}
+
+const stateMachine = new StateMachine({ isActive: false }, (input, state) => {
+  const newState = { isActive: !state.isActive };
+  return {
+    output: newState,
+    state: newState,
+  };
+});
+
+// Example template function
+const template = (input, driver) => {
+  const d = drive(driver);
+  return html`<button @click=${d} @wheel=${d}>
+    Toggle : ${input.isActive}
+  </button>`;
+};
+
+const component = new Component(template, stateMachine);
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+module.exports = {
+  root: "./src", // root directory of the project
+  base: "./", // base path for the project
+  server: {
+    open: true, // automatically open the browser
+    cors: true, // enable CORS
+  },
+};
